2f59b690636b9672771899d93349771e679ab5f8
Erodraeda <lucaswalterm@gmail.com>
2021-11-21 19:45:33 -0300

    Added score system to tests

--- recuperacao/testModelo.java
+++ recuperacao/testModelo.java
@@ -4,36 +4,61 @@ import java.io.IOException;
 
 public class testModelo {
 
+    private static int testValue = 0;
+
     public static void testes() throws IOException {
         Autor autor1 = new Autor("Lucas", "21", 1);
-        if (autor1.getId() == 1)
+        if (autor1.getId() == 1) {
             System.out.println("Autor 1 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 1 existe? " + false);
 
         Autor autor2 = new Autor("Alice", "18", 2);
-        if (autor2.getId() == 2)
+        if (autor2.getId() == 2) {
             System.out.println("Autor 2 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 2 existe? " + false);
 
         Autor autor3 = new Autor("Thiago", "30", 3);
-        if (autor3.getId() == 3)
+        if (autor3.getId() == 3) {
             System.out.println("Autor 3 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 3 existe? " + false);
 
         System.out.println(autor1.getId());
+        if (autor1.getId() == 1)
+            testValue++;
         System.out.println(autor1.getNome());
+        if (autor1.getNome() == "Lucas")
+            testValue++;
         System.out.println(autor1.getIdade());
+        if (autor1.getIdade() == "21")
+            testValue++;
 
         System.out.println(autor2.getId());
+        if (autor2.getId() == 2)
+            testValue++;
         System.out.println(autor2.getNome());
+        if (autor2.getNome() == "Alice")
+            testValue++;
         System.out.println(autor2.getIdade());
+        if (autor2.getIdade() == "18")
+            testValue++;
 
         System.out.println(autor3.getId());
+        if (autor3.getId() == 3)
+            testValue++;
         System.out.println(autor3.getNome());
+        if (autor3.getNome() == "Thiago")
+            testValue++;
         System.out.println(autor3.getIdade());
+        if (autor3.getIdade() == "30")
+            testValue++;
+
+        System.out.println("Valor total nos testes: " + testValue + "/12");
 
     }
 
--- recuperacao/testPersistencia.java
+++ recuperacao/testPersistencia.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 public class testPersistencia {
 
     private static Path authorDir = Paths.get("files", "Authors.csv");
+    private static int testValue = 0;
 
     public static void testes() throws IOException {
 
@@ -15,55 +16,73 @@ public class testPersistencia {
         if (Files.exists(dir)) {
             Files.delete(dir);
             Files.createFile(dir);
+            testValue++;
         } else {
             Files.createFile(dir);
+            testValue++;
         }
 
         Autor autor1 = new Autor("Lucas", "21", 1);
-        if (autor1.getId() == 1)
+        if (autor1.getId() == 1) {
             System.out.println("Autor 1 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 1 existe? " + false);
 
         new arquivosAutor(autor1.getNome(), autor1.getIdade(), autor1.getId());
-        if (arquivosAutor.getAutor(1) != null)
+        if (arquivosAutor.getAutor(1) != null) {
             System.out.println("Autor 1 " + arquivosAutor.getAutor(1));
+            testValue++;
+        }
 
         Autor autor2 = new Autor("Alice", "18", 2);
-        if (autor2.getId() == 2)
+        if (autor2.getId() == 2) {
             System.out.println("Autor 2 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 2 existe? " + false);
 
         new arquivosAutor(autor2.getNome(), autor2.getIdade(), autor2.getId());
-        if (arquivosAutor.getAutor(2) != null)
+        if (arquivosAutor.getAutor(2) != null) {
             System.out.println("Autor 2 " + arquivosAutor.getAutor(2));
+            testValue++;
+        }
 
         Autor autor3 = new Autor("Thiago", "30", 3);
-        if (autor3.getId() == 3)
+        if (autor3.getId() == 3) {
             System.out.println("Autor 3 existe? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor 3 existe? " + false);
 
         new arquivosAutor(autor3.getNome(), autor3.getIdade(), autor3.getId());
-        if (arquivosAutor.getAutor(3) != null)
+        if (arquivosAutor.getAutor(3) != null) {
             System.out.println("Autor 3 " + arquivosAutor.getAutor(3));
+            testValue++;
+        }
 
         arquivosAutor.listar();
-        if (!arquivosAutor.listar().isEmpty())
+        if (!arquivosAutor.listar().isEmpty()) {
             System.out.println("Lista de autores vazia? " + false + "\n" + arquivosAutor.listar());
-        else
+            testValue++;
+        } else
             System.out.println("Lista de autores vazia? " + true);
 
         arquivosAutor.updateAuthor(3, "Tiago", "32");
         System.out.println("Autor editado: " + arquivosAutor.getAutor(3));
+        if (arquivosAutor.getAutor(3).get(0).equals("3;Tiago;32;")) {
+            testValue++;
+        }
 
         arquivosAutor.deleteAuthor(2);
         System.out.println("Autor deletado: " + arquivosAutor.getAutor(2));
-        if (arquivosAutor.getAutor(2).isEmpty())
+        if (arquivosAutor.getAutor(2).isEmpty()) {
             System.out.println("Autor Deletado? " + true);
-        else
+            testValue++;
+        } else
             System.out.println("Autor Deletado? " + false);
+
+        System.out.println("Valor total nos testes: " + testValue + "/10");
 
         System.exit(1);
     }
